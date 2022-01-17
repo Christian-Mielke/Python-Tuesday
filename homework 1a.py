21e68122eea60c95903abce0544f6e7a842b87e6
Kevin Hausmann <kevin.hausmann@uba.de>
2022-01-17 08:47:52 +0100

    First try

---
+++ homework 1a.py
@@ -0,0 +1,13 @@
+import random
+
+print('Willkommen zum Zahlenratespiel! Ich habe mir eine Zahl zwischen 0 und 100 ausgedacht.')
+number: int = random.randint(0, 100)
+guess: int = -1
+
+while number != guess:
+    guess = int(input('Rate meine Zahl!'))
+    
+    if number != guess:
+        print(f'Guter Tipp! Aber meine Zahl ist {"größer" if number > guess else "kleiner"}...')
+
+print(f'Sehr gut! Meine Zahl war {number}.')
