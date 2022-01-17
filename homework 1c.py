5124ed83d019539f490d1b1651c21e4bbe95e954
Kevin Hausmann <kevin.hausmann@uba.de>
2022-01-17 09:52:41 +0100

    Another version, pulling out user input to function

---
+++ homework 1c.py
@@ -0,0 +1,18 @@
+import random
+
+print('Willkommen zum Zahlenratespiel! Ich habe mir eine Zahl zwischen 0 und 100 ausgedacht.')
+number: int = random.randint(0, 100)
+attempts: list = []
+
+def ask_for_guess(previous_attempts: list) -> int:
+    user_input = int(input('Rate meine Zahl!'))
+    previous_attempts += [user_input]
+    
+    return user_input
+
+guess: int = ask_for_guess(attempts)
+while number != guess:
+    print(f'Guter Tipp! Aber meine Zahl ist {"größer" if number > guess else "kleiner"}...')
+    guess = ask_for_guess(attempts)
+
+print(f'Sehr gut! Meine Zahl war {number}. Du hast {len(attempts)} Versuch(e) gebraucht: {attempts}')
