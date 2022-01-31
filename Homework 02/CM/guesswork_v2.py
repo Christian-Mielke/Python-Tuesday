af8b07950ce0b193b577056bd2a3e45a5dd048c7
Christian-Mielke <79509063+Christian-Mielke@users.noreply.github.com>
2022-01-31 10:58:18 +0100

    Removed the pesky while true
    and removed the verbose clutter.

--- Homework 02/CM/guesswork_v2.py
+++ Homework 02/CM/guesswork_v2.py
@@ -3,7 +3,6 @@
 """
 ***************************************************************************
     guesswork.py
-
 This is a small guessing game for your computer published at:
 DXDOI:JourLaugh010422/0815/12345
     ---------------------
@@ -31,7 +30,6 @@ class Guestimation:
         Class that contains a number guessing game
         PC or Human chooses integer between start and top range
         User or PC needs to do the guessing.
-
         :inits guestimation object with start,stop and choice attributes
         :param start: Lowest possible integer for a random integer Defaults to 0
         :param stop: Highest possible integer for a random integer Defaults to 100
@@ -41,23 +39,23 @@ class Guestimation:
             f'Welcome to the guessing game either you or your computer needs to guess which number has been chosen out of a randint({start},{stop})')
         print('\n')
         self.__choice = None
+        self.gametype = None
         self.start: int = start
         self.stop: int = stop
         options: list = ['P', 'H']
-    while self.gametype not in options:
-            self.gametype = input('Please input P for PC guestimation or H for Human guestimation!')
-            print('Please input P for PC guestimation or H for Human guestimation!')
-            self.gametype = input()
-            if self.gametype in options:
-                break
-            else:
-                print('Error! Please input P for PC guestimation or H for Human guestimation!')
+        while self.gametype not in options:
+                self.gametype = input('Please input P for PC guestimation or H for Human guestimation!')
+                print('Please input P for PC guestimation or H for Human guestimation!')
+                self.gametype = input()
+                if self.gametype in options:
+                    break
+                else:
+                    print('Error! Please input P for PC guestimation or H for Human guestimation!')
 
     @staticmethod
     def check_number(number: int, start: int, stop: int):
         """
         Classmethod for checking the user input with respect to the data range
-
         :param number: User input Number
         :param start: Lowest possible integer for a random integer Defaults to 0
         :param stop: Highest possible integer for a random integer Defaults to 100
@@ -89,22 +87,19 @@ class Guestimation:
         Classmethod that runs the human guestimation game in a loop.
         Exits loop if number guessed correctly
         """
-        print("I will pick my number now!")
         self.__choice = random.randint(self.start, self.stop)
-        while True:
-            print(f'Please input an integer from {self.start} to {self.stop} to guess your PCs Random Choice!')
+        guess = "start"
+        print(f'Please input an integer from {self.start} to {self.stop} to guess your PCs Random Choice!')
+        while guess != self.__choice:
             guess = input()
             guess = Guestimation.check_int(guess)
             if guess == -1:
-                print("Error was encoutered try to punch in integer only again!")
                 continue
             t_val = Guestimation.check_number(guess, self.start, self.stop)
             if t_val == -1:
-                print("Error was encoutered try again!")
                 continue
             if guess == self.__choice:
                 print(f'{guess} was the correct answer!')
-                break
             elif guess < self.__choice:
                 print(f'{guess} is smaller than your PCs choice')
             elif guess > self.__choice:
@@ -113,29 +108,25 @@ class Guestimation:
     def play_pc(self):
         print(f"Please think of a number between {self.start} and {self.stop}")
         print(f"and give me hints for smaller=-1,larger=1 and match=0 to guess your number!")
-        while True:
+        checkmate_aux = None
+        while checkmate_aux != "0":
             datarange = numpy.arange(self.start, self.stop + 1, 1)
             guessidx = len(datarange) // 2
             if guessidx == 1:
-                print(f"Is your number {datarange[0]}? [0] Or is it smaller=[-1] or larger[1]")
+                print(f"Is your number {datarange[0]}? ")
                 checkmate = input()
                 checkmate_aux = Guestimation.check_checkmate(checkmate)
                 if checkmate_aux == "Error":
                     continue
                 if checkmate_aux == "0":
                     print(f"I won your number was {datarange[0]}")
-                    break
-                else:
-                    print(f"I won your number was {datarange[1]}")
-                    break
-            print(f"Is your number {datarange[guessidx]}? [0] Or is it smaller=[-1] or larger[1]")
+            print(f"Is your number {datarange[guessidx]}?")
             checkmate = input()
             checkmate_aux = Guestimation.check_checkmate(checkmate)
             if checkmate_aux == "Error":
                 continue
             if checkmate == "0":
                 print(f"I won your number was {datarange[guessidx]}")
-                break
             elif checkmate == "-1":
                 self.stop = datarange[guessidx]
             elif checkmate == "1":
